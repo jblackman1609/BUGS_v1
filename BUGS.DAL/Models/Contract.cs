66cd289dfd9a2888a6702c2ac3c31b9c1f21e029
Joe <jblackman1609@gmail.com>
2026-08-03 22:45:48 -0500

    Modified Contract model.

--- BUGS.DAL/Models/Contract.cs
+++ BUGS.DAL/Models/Contract.cs
@@ -2,7 +2,7 @@ namespace BUGS.DAL.Models;
 
 public class Contract
 {
-    public required string Id { get; set; }
+    public string Id { get; set; } = Guid.NewGuid().ToString();
     public Customer Customer { get; set; } = new();
     public string PropertyType { get; set; } = string.Empty;
     public string StreetAddress { get; set; } = string.Empty;
@@ -10,6 +10,7 @@ public class Contract
     public string State { get; set; } = string.Empty;
     public string ZipCode { get; set; } = string.Empty;
     public string BondType { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
     public DateTime EffectiveDate { get; set; }
     public DateTime ContractEndDate { get; set; }
     public DateTime TimeStamp { get; set; }
