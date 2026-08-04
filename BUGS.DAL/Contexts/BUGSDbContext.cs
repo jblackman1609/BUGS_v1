d963c7f81539022554bbebea941e6618dec803eb
Joe <jblackman1609@gmail.com>
2026-08-03 22:45:23 -0500

    Added correct context.

---
+++ BUGS.DAL/Contexts/BUGSDbContext.cs
@@ -0,0 +1,16 @@
+using BUGS.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BUGS.DAL.Contexts;
+
+public class BUGSDbContext : DbContext
+{
+    public DbSet<Contract> Contracts { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    public BUGSDbContext() {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+    optionsBuilder.UseSqlite("Data Source=bugs.db");
+    
+}
